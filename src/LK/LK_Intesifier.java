1b965e20015b8d6e5642d7b205363b4e6c126ee5
usmcuper@gmail.com <usmcuper@gmail.com@66591f0f-0a03-a583-64e9-e3b0b593df45>
2013-11-18 22:37:07 +0000

    Francesco Creazione package LK e classe per l'algoritmo intensificatore Lin-Kernighan. Classe già utilizzabile, implementazione dummy.

---
+++ src/LK/LK_Intesifier.java
@@ -0,0 +1,14 @@
+package LK;
+
+import tsp.model.Intensifier;
+import tsp.model.Solution;
+
+public class LK_Intesifier implements Intensifier {
+
+	@Override
+	public Solution improve(Solution start) {
+		// TODO Auto-generated method stub
+		return start;
+	}
+
+}
