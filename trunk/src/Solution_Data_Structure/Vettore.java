1b215c57837518b209dd181d9f25cf25b161c24c
agnerondi <agnerondi@66591f0f-0a03-a583-64e9-e3b0b593df45>
2013-11-19 21:31:11 +0000

    Implementazione soluzione con vettore

--- trunk/src/Solution_Data_Structure/Vettore.java
+++ trunk/src/Solution_Data_Structure/Vettore.java
@@ -1,41 +1,61 @@
 package Solution_Data_Structure;
 
+import java.util.*;
+
 import tsp.model.City;
 import tsp.model.Solution;
 
 public class Vettore implements Solution {
 	
-		
+	Map<City,Integer> appoggio; // in questo vettore memorizzo l'associazione tra la cità e la sua posizione nel vettore
+    City[] soluzione;
+    int costo;
 	@Override
 	public City next(City c) {
 		// TODO Auto-generated method stub
-		return null;
-		
+		int p=appoggio.get(c);
+		return soluzione[p+1];
 	}
 
 	@Override
 	public City previous(City c) {
 		// TODO Auto-generated method stub
-		return null;
+		int p=appoggio.get(c);
+		return soluzione[p-1];
 	}
-
 	@Override
 	public boolean between(City a, City b, City c) {
 		// TODO Auto-generated method stub
-		return false;
+		boolean f=false;
+		int pa=appoggio.get(a);
+		int pb=appoggio.get(b);
+		int pc=appoggio.get(c);
+		if( pa<pb && pb<pc){
+			f=true;
+		}
+		return f;
 	}
 
 	@Override
 	public void flip(City a, City b) {
 		// TODO Auto-generated method stub
-		
+		int pa=appoggio.get(a);
+		int pb=appoggio.get(b);
+		soluzione[pa]=b;
+		soluzione[pb]=a;
+		appoggio.put(a, pb);
+		appoggio.put(b, pa);
+		// calcolo la differenza di costo
 	}
 
 	@Override
 	public int length() {
 		// TODO Auto-generated method stub
-		return 0;
+		return costo;
 	}
+
+	
+	
 
 	
 
