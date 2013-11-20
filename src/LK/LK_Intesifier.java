b50b5a03fa2825ef529e8a4cf6a8b3cc364ecb0a
usmcuper <usmcuper@66591f0f-0a03-a583-64e9-e3b0b593df45>
2013-11-20 17:20:26 +0000

    Modifica classe City: 1)coordinate espresse in double invece che int 2)implementazione del metodo distance e aggiunta del metodo distance_squared (quadrato della distanza)
    Aggiunta classe Edge: modello di arco (direzionato)

--- src/LK/LK_Intesifier.java
+++ src/LK/LK_Intesifier.java
@@ -1,14 +1,22 @@
 package LK;
 
-import tsp.model.Intensifier;
-import tsp.model.Solution;
+import tsp.model.*;
+
 
 public class LK_Intesifier implements Intensifier {
+	
+	// miglior guadagno registrato
+	int best_gain = 0;
+	
+	// miglior tour registrato
+	Solution best_solution = null;
 
 	@Override
 	public Solution improve(Solution start) {
-		// TODO Auto-generated method stub
-		return start;
+		
+		best_solution = start;
+		
+		return best_solution;
 	}
 
 }
