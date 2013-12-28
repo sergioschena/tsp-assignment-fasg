38f2e5bd1836ae554c7ff9544998494ac6e90e9f
sergio.schena <sergio.schena@66591f0f-0a03-a583-64e9-e3b0b593df45>
2013-12-28 10:12:27 +0000

    The end

--- src/tsp/model/CityManager.java
+++ src/tsp/model/CityManager.java
@@ -79,10 +79,9 @@ public class CityManager {
 	private static int distance(City a, City b){
 		double xd = a.getX() - b.getX();
 		double yd = a.getY() - b.getY();
-		// FIXME: cambiare calcolo distanza
 		// TSPLIB requires a nearest-int rounding
-		// return (int) Math.round(Math.sqrt(xd*xd + yd*yd));
-		return (int) Math.ceil(Math.sqrt(xd*xd + yd*yd));
+		return (int) Math.round(Math.sqrt(xd*xd + yd*yd));
+		//return (int) Math.ceil(Math.sqrt(xd*xd + yd*yd));
 	}
 	
 	private City[] bestNearestOf(City c){
